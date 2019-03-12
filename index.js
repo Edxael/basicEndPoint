62fefbb1f7d6c36f0526e5c662ac1b4ba97ae310
Edmundo rubio <edmundo@webaholics.co>
2019-03-11 19:55:17 -0600

    create array of items

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const app = express()
 
+let items = [
+  {
+    name: 'computer',
+    price: 300,
+    image: 'https://i5.walmartimages.com/asr/3826f75b-efeb-4a76-bd3a-735641a4a1c9_1.b84fd4ece51b32016a10c20ef07199cf.jpeg'
+  },
+  {
+    name: 'car',
+    price: 3000,
+    image: 'https://img.newatlas.com/mazda-vision-coupe-wins-2018-concept-car-of-the-year-4.jpg?auto=format%2Ccompress&ch=Width%2CDPR&fit=crop&h=347&q=60&rect=0%2C106%2C1620%2C912&w=616&s=a40e38fdb6995f043b03589dd2e2bf6e'
+  },
+  {
+    name: 'house',
+    price: 300000,
+    image: 'https://img.huffingtonpost.com/asset/5a3126821500004e0049b6c9.jpeg?ops=scalefit_720_noupscale'
+  },
+]
+
 app.get('/test', (req, res) => {
   res.json({ data: 'test data' })
 })
 
-app.get('/item', (req, res) => {
-  res.json({
-    name: 'computer',
-    price: 300,
-    image: 'https://i5.walmartimages.com/asr/3826f75b-efeb-4a76-bd3a-735641a4a1c9_1.b84fd4ece51b32016a10c20ef07199cf.jpeg'
+app.get('/items', (req, res) => {
+  res.send({
+    message: 'success',
+    data: items
   })
 })
 
