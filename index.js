e4651dd83af0c779f1d47dd2308375015836d4f8
Edmundo rubio <edmundo@webaholics.co>
2019-03-13 19:42:02 -0600

    add Id to the items

--- index.js
+++ index.js
@@ -8,16 +8,19 @@ let userCart = []
 
 let items = [
   {
+    id: 1,
     name: 'computer',
     price: 300,
     image: 'https://i5.walmartimages.com/asr/3826f75b-efeb-4a76-bd3a-735641a4a1c9_1.b84fd4ece51b32016a10c20ef07199cf.jpeg'
   },
   {
+    id: 2,
     name: 'car',
     price: 3000,
     image: 'https://img.newatlas.com/mazda-vision-coupe-wins-2018-concept-car-of-the-year-4.jpg?auto=format%2Ccompress&ch=Width%2CDPR&fit=crop&h=347&q=60&rect=0%2C106%2C1620%2C912&w=616&s=a40e38fdb6995f043b03589dd2e2bf6e'
   },
   {
+    id: 3,
     name: 'house',
     price: 300000,
     image: 'https://img.huffingtonpost.com/asset/5a3126821500004e0049b6c9.jpeg?ops=scalefit_720_noupscale'
